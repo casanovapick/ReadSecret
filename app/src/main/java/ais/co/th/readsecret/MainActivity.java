e3bbb5c86ef24d39e46bafbd1a623b5686098773
Jirachai Hongthong <jirachai_h1@hotmail.com>
2015-03-18 14:58:37 +0700

    UI,Logout

--- app/src/main/java/ais/co/th/readsecret/MainActivity.java
+++ app/src/main/java/ais/co/th/readsecret/MainActivity.java
@@ -2,7 +2,10 @@ package ais.co.th.readsecret;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,21 +15,44 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity {
     Button btnGET;
     TextView txtSecret;
-
+    Button btnOut;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs =  PreferenceManager.getDefaultSharedPreferences(this);
+
         btnGET = (Button) findViewById(R.id.btn_get);
+        btnOut = (Button) findViewById(R.id.btn_logout);
         txtSecret = (TextView) findViewById(R.id.txt_secret);
+
         btnGET.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setComponent(new ComponentName("ais.co.th.writesecret", "ais.co.th.writesecret.MainActivity"));
+                intent.setComponent(new ComponentName("ais.co.th.writesecret", "ais.co.th.writesecret.ConfirmActivity"));
                 startActivityForResult(intent,200);
             }
         });
+
+        btnOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefs.edit().putString("Secret","").apply();
+                finish();
+            }
+        });
+        if(prefs.getString("Secret","").matches("")){
+            btnOut.setVisibility(View.GONE);
+            btnGET.setVisibility(View.VISIBLE);
+
+        }else{
+            txtSecret.setText("Login with "+prefs.getString("Secret",""));
+
+            btnGET.setVisibility(View.GONE);
+            btnOut.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -34,7 +60,10 @@ public class MainActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (200 == requestCode) {
             if (resultCode == RESULT_OK) {
-                txtSecret.setText(data.getStringExtra("Secret"));
+                txtSecret.setText("Login with "+data.getStringExtra("Secret"));
+                prefs.edit().putString("Secret", data.getStringExtra("Secret")).apply();
+                btnGET.setVisibility(View.GONE);
+                btnOut.setVisibility(View.VISIBLE);
             }
         }
     }
